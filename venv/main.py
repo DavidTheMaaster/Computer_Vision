9ca61974c29926dbd1dbb512236ae3f9c5957b0f
DavidLozanoSánchez <davidthemaaster@gmail.com>
2019-03-31 14:11:25 +0200

    Make some performance improvement

--- venv/main.py
+++ venv/main.py
@@ -6,11 +6,19 @@ def execute():
     target_img_path = "Img/t1-img2.png"
     threshold = 0.1
 
-    input_img = np.float64(cv2.imread(input_img_path, cv2.COLOR_BGR2GRAY))
-    target_img = np.float64(cv2.imread(target_img_path,cv2.COLOR_BGR2GRAY))
+    input_img = cv2.imread(input_img_path)
+    target_img = cv2.imread(target_img_path)
 
-    Irows, Icols = input_img.shape[:2]
-    Trows, Tcols = target_img.shape[:2]
+    #get the images on grey scale for better performance
+    input_grey = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+    target_grey = cv2.cvtColor(target_img, cv2.COLOR_BGR2GRAY)
+
+    #scaled the images for better performance
+    input_grey = cv2.resize(input_grey, (0, 0), fx=0.5, fy=0.5);
+    target_grey = cv2.resize(target_grey, (0, 0), fx=0.5, fy=0.5);
+
+    Irows, Icols = input_grey.shape
+    Trows, Tcols = target_grey.shape
 
     Mrows = Irows - Trows + 1
     Mcols = Icols - Tcols + 1
@@ -19,44 +27,45 @@ def execute():
 
     # calculates the matchin map
 
-    pixel_sum = 0
+    pixel_min = 0
 
     for i in range(0, Mrows):
         for j in range(0, Mcols):
-            pixel = matching_map_minimum(input_img, target_img, i, j)
-            pixel_sum += pixel
+            pixel = squared_difference(input_grey, target_grey, i, j)
+            pixel_min += pixel
+            pixel_max = normalized_squared_difference(input_grey, target_grey, Trows, Tcols, i, j)
 
-            matching_img[i, j] = pixel_sum / matching_map_maximum(input_img, target_img, Trows, Tcols, i, j)
+            matching_img[i, j] = pixel_min / pixel_max
 
             if matching_img[i, j] < threshold:
                 #detects when the value is less than the threshold (found taret image)
                 print("patata")
-            pixel_sum = 0
+            pixel_min = 0
+
+    matching_img = cv2.resize(matching_img, (0, 0), fx=2, fy=2);
 
     cv2.imshow("Input image", np.uint8(input_img))
     cv2.imshow("Matching image", np.uint8(matching_img))
     cv2.imshow("Target image", np.uint8(target_img))
     cv2.waitKey(0)
 
-def matching_map_minimum(input, target, i, j):
-    #calculates the minimum value of the matching map
+def squared_difference(input, target, i, j):
+    #calculates the squared diference
 
     Trows, Tcols = target.shape[:2]
     pixel_sum = 0
     for x in range(0, Trows):
         for y in range(0, Tcols):
-            pixel = (target[x, y, 2] - input[i + x, j + y, 2]) ** 2
+            pixel = (target[x, y] - input[i + x, j + y]) ** 2
             pixel_sum += pixel
 
     return  pixel_sum
 
 
-def matching_map_maximum(input, target, rows, cols, i, j):
-    #calculates the maximum value of the matching map
-    return np.sqrt(((target[i:i + rows, j:j + cols, 2]) ** 2).sum(axis=(0, 1)) - (((input[i:i + rows, j:j + cols, 2]) ** 2).sum(axis=(0, 1))))
+def normalized_squared_difference(input, target, rows, cols, i, j):
+    #calculates normalized_squared diference
+    return np.sqrt(((target[i:i + rows, j:j + cols]) ** 2).sum(axis=(0, 1)) - (((input[i:i + rows, j:j + cols]) ** 2).sum(axis=(0, 1))))
 
-def normalize():
-    PATATA = 0
 
 if __name__ == '__main__':
     execute()
